12ff3b7a6c45311401c88479b9ff034a37ba602b
Vasanthakumar Vijayasekaran <vasanth260m12@gmail.com>
2023-02-26 16:12:41 +0530

    Extract vector dot and sum into functions

--- src/approx_line_search.rs
+++ src/approx_line_search.rs
@@ -19,8 +19,7 @@ fn bracktracking_line_search<const N: usize>(
 
     let (y, g) = (f(x), grad_f(x));
     loop {
-        let suff_decr = y + (beta * alpha * g.zip(dir).map(|(g, d)| g * d).iter().sum::<f32>());
-        if f(x.zip(dir).map(|(x, d)| x + alpha * d)) <= suff_decr {
+        if f(sum(x, dir.map(|d| alpha * d))) <= y + (beta * alpha * dot(g, dir)) {
             break;
         }
         alpha *= p;
@@ -48,23 +47,14 @@ fn strong_bracktracking<const N: usize>(
         sigma,
     } = config;
 
-    let (y0, g0, mut y_prev, mut alpha_prev) = (
-        f(x),
-        grad_f(x).zip(d).map(|(g, d)| g * d).iter().sum::<f32>(),
-        None,
-        0.,
-    );
+    let (y0, g0, mut y_prev, mut alpha_prev) = (f(x), dot(grad_f(x), d), None, 0.);
 
     let (mut alpha_lo, mut alpha_hi) = loop {
-        let y = f(x.zip(d).map(|(x, d)| x + alpha * d));
+        let y = f(sum(x, d.map(|d| alpha * d)));
         if y > y0 + beta * alpha * g0 || (y_prev.is_some() && y >= y_prev.unwrap()) {
             break (alpha_prev, alpha);
         }
-        let g: f32 = grad_f(x.zip(d).map(|(x, d)| x + alpha * d))
-            .zip(d)
-            .map(|(g, d)| g * d)
-            .iter()
-            .sum();
+        let g = dot(grad_f(sum(x, d.map(|d| alpha * d))), d);
         if g.abs() <= -sigma * g0 {
             return alpha;
         } else if g >= 0. {
@@ -73,18 +63,14 @@ fn strong_bracktracking<const N: usize>(
         (y_prev, alpha_prev, alpha) = (Some(y), alpha, 2. * alpha);
     };
 
-    let y_lo = f(x.zip(d).map(|(x, d)| x + alpha_lo * d));
+    let y_lo = f(sum(x, d.map(|d| alpha_lo * d)));
     loop {
         let alpha = (alpha_lo + alpha_hi) / 2.;
-        let y = f(x.zip(d).map(|(x, d)| x + alpha * d));
+        let y = f(sum(x, d.map(|d| alpha * d)));
         if y > y0 + beta * alpha * g0 || y >= y_lo {
             alpha_hi = alpha;
         } else {
-            let g: f32 = grad_f(x.zip(d).map(|(x, d)| x + alpha * d))
-                .zip(d)
-                .map(|(g, d)| g * d)
-                .iter()
-                .sum();
+            let g = dot(grad_f(sum(x, d.map(|d| alpha * d))), d);
             if g.abs() <= -sigma * g0 {
                 return alpha;
             } else if g * (alpha_hi - alpha_lo) >= 0. {
@@ -93,4 +79,12 @@ fn strong_bracktracking<const N: usize>(
             alpha_lo = alpha;
         }
     }
+}
+
+fn dot<const N: usize>(a: [f32; N], b: [f32; N]) -> f32 {
+    a.zip(b).map(|(a, b)| a * b).iter().sum()
+}
+
+fn sum<const N: usize>(a: [f32; N], b: [f32; N]) -> [f32; N] {
+    a.zip(b).map(|(a, b)| a + b)
 }
